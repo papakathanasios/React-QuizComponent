fcb519a875ccc2a69e65ca0b1f4a72d65d0d7352
Thanos Papakostas <papakostas.athanasios@nbg.gr>
2018-06-11 14:35:25 +0300

    2nd module

--- src/Quiz.js
+++ src/Quiz.js
@@ -5,10 +5,10 @@ let quizData = require('./quiz_data.json')
 class Quiz extends Component{
     constructor(props){
         super(props)
-        this.state = {quiz_position: 1}
+        this.state = { quiz_position: 1 }
     }
     render(){
-        reutrn(<div>Quiz</div>)
+        return(<div><div className="QuizQuestion">{quizData.quiz_questions[0].instruction_text}</div></div>)
     }
 }
 
