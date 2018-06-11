8a14261b729ca407a5453a322700c41438b27b93
Thanos Papakostas <papakostas.athanasios@nbg.gr>
2018-06-11 14:28:15 +0300

    test

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,3 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
-let quizData = require('./quiz_data.json');
+let quizData = require('./quiz_data.json')
+
+class Quiz extends Component{
+    constructor(props){
+        super(props)
+        this.state = {quiz_position: 1}
+    }
+    render(){
+        reutrn(<div>Quiz</div>)
+    }
+}
+
+export default Quiz
