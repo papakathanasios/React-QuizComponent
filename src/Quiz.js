b9941b83e69b0c43378faedf466b54c6e5e5927d
Thanos Papakostas <papakostas.athanasios@nbg.gr>
2018-06-12 09:48:09 +0300

    5 v1

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import QuizQuestion from './QuizQuestion.js'
+import QuizQuestion from './QuizEnd.js'
+import QuizEnd from './QuizEnd.js';
 
 
 let quizData = require('./quiz_data.json')
@@ -10,7 +12,10 @@ class Quiz extends Component{
         this.state = { quiz_position: 1 }
     }
     render(){
-        return(<div><QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position -1]} /></div>)
+        
+        return(<div>
+            <QuizEnd />
+            <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position -1]} /></div>)
     }
 }
 
