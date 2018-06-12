c417d3b48ac767924aa9d5c22429179f0b35ffaa
Thanos Papakostas <papakostas.athanasios@nbg.gr>
2018-06-12 09:39:22 +0300

    4

--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -2,8 +2,7 @@ import React, { Component } from 'react'
 
 class QuizQuestionButton extends Component{
     render(){
-        return(<li>
-                <button>{this.props.quiz_question.answer_options[0]}</button></li>)
+        return(<li><button></button></li>)
     }
 }
 
